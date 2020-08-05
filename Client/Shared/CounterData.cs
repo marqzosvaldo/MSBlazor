b7fdd9ef4d01edd5ffb49b8806756c43c1d8ba9c
marqzosvaldo <crosser.xr@gmail.com>
2020-08-04 20:52:23 -0500

    communicating with  cascading paramters

---
+++ Client/Shared/CounterData.cs
@@ -0,0 +1,17 @@
+using System;
+namespace MSBlazor.Client.Shared {
+    public class CounterData {
+        private int count;
+        public int Count {
+            get => this.count;
+            set {
+                if (value != this.count) {
+                    this.count = value;
+                    CountChanged?.Invoke(this.count);
+                }
+            }
+        }
+
+        public Action<int> CountChanged { get; set; }
+    }
+}
