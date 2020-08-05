171a8df9c0d80b4217be98fef6fb758b39c94820
marqzosvaldo <crosser.xr@gmail.com>
2020-08-04 23:16:53 -0500

    sign RequireConfirmedAccount

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -33,7 +33,7 @@ namespace MSBlazor.Server
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
